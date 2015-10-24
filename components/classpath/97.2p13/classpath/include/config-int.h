ad0a887ca15fb7c1944e831bf61b5d1ac4f867f2
Alan Weide <alan.d.weide@gmail.com>
2015-10-24 01:11:35 -0400

    Initial commit: bare Jikes RVM

---
+++ components/classpath/97.2p13/classpath/include/config-int.h
@@ -0,0 +1,9 @@
+#ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
+#define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "classpath 0.97.2"
+/* generated using gnu compiler gcc (Ubuntu 4.8.4-2ubuntu1~14.04) 4.8.4 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
