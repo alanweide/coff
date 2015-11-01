98cd781c95764d81c262a643adbb51762bc2b92d
Alan Weide <alan.d.weide@gmail.com>
2015-11-01 17:21:04 -0500

    Test.java outputs the time it took; coff flag in properties

--- coff/src/coff/Test.java
+++ coff/src/coff/Test.java
@@ -1,27 +1,31 @@
 package coff;
 
+import java.util.Date;
+
 public class Test {
 	private static void a() {
 		for (long i = 0; i < 10000000000L; i++) {
-			
+
 		}
 	}
-	
+
 	private static void b() {
 		for (long i = 0; i < 9000000000L; i++) {
-			
+
 		}
 	}
+
 	public static void main(String[] args) throws InterruptedException {
+		Date start = new Date();
 		Thread a = new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				a();
 			}
 		});
 		Thread b = new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				b();
@@ -31,5 +35,7 @@ public class Test {
 		b.run();
 		a.join();
 		b.join();
+		double secondsElapsed = (new Date().getTime() - start.getTime()) / 1000.0;
+		System.out.println("Ran test in " + secondsElapsed + " seconds");
 	}
 }
