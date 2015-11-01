4578318d3026dc183e3dba39bb72629aa4c417c3
Alan Weide <alan.d.weide@gmail.com>
2015-11-01 14:58:13 -0500

    actually removed the bad files and updated gitignore

--- dist/production_x86_64-linux/include/RunBootImage.h
+++
@@ -1,8 +0,0 @@
-
-      const unsigned heap_default_initial_size = 50*1024*1024;
-      const unsigned heap_default_maximum_size = 100*1024*1024;
-      const char *rvm_version = "Jikes RVM 3.1.3 (revision 10592:4cb2f9536054)";
-      const char *rvm_configuration = "production";
-      const char *rvm_host_configuration = "/home/alan/Developer/jikesrvm/build/configs/production.properties";
-      const char *rvm_target_configuration = "/home/alan/Developer/jikesrvm/build/targets/x86_64-linux.properties";
-    
